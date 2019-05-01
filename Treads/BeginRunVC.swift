1abe4a95c95142a01cd6a95f0560f9372e0ae923
Admin <walidelzo@gmail.com>
2019-05-01 21:13:27 +0200

    init commit

---
+++ Treads/BeginRunVC.swift
@@ -0,0 +1,20 @@
+//
+//  BeginRunVC.swift
+//  Treads
+//
+//  Created by Admin on 5/1/19.
+//  Copyright © 2019 NanoSoft. All rights reserved.
+//
+
+import UIKit
+
+class BeginRunVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treads/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treads
+//
+//  Created by Admin on 5/1/19.
+//  Copyright © 2019 NanoSoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
